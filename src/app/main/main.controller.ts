07268d7e8d65859da0ba280d7fb35aa2fd71a55b
Everett Wolf <everettwolf@gmail.com>
2015-09-27 19:20:40 -0700

    cleanup

--- src/app/main/main.controller.ts
+++ src/app/main/main.controller.ts
@@ -2,36 +2,14 @@ module btcService {
   'use strict';
 
   export class MainController {
-    public awesomeThings: ITecThing[];
-    public webDevTec: WebDevTecService;
-    public classAnimation: string;
 
     /* @ngInject */
-    constructor ($timeout: ng.ITimeoutService, webDevTec: WebDevTecService, toastr: Toastr) {
-      this.awesomeThings = new Array();
-      this.webDevTec = webDevTec;
-      this.classAnimation = '';
+    constructor ($timeout: ng.ITimeoutService) {
 
-      this.activate($timeout);
+          // do nothing
+
+
     }
 
-    activate($timeout: ng.ITimeoutService) {
-      this.getWebDevTec();
-
-      var self = this;
-
-      $timeout(function() {
-        self.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      this.classAnimation = '';
-    }
-
-    getWebDevTec() {
-      this.awesomeThings = this.webDevTec.tec;
-    }
   }
 }
